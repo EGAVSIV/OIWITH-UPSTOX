8dfbee7363ea3a8550aa7e6cadaef246d4a8652d
Gaurav Singh Yadav <137716439+EGAVSIV@users.noreply.github.com>
2025-12-09 20:28:51 +0530

    Update oidecay.py

--- oidecay.py
+++ oidecay.py
@@ -10,7 +10,7 @@ from datetime import datetime
 # ---------------------------------------------
 st.set_page_config(page_title="Global OTM OI Decay Scanner", layout="wide")
 
-ACCESS_TOKEN = "<PUT YOUR TOKEN HERE>"
+ACCESS_TOKEN = "eyJ0eXAiOiJKV1QiLCJrZXlfaWQiOiJza192MS4wIiwiYWxnIjoiSFMyNTYifQ.eyJzdWIiOiIxMjU5MDciLCJqdGkiOiI2OTM3OWIyMDI0Njk1MjJkYTE1MjlkZDMiLCJpc011bHRpQ2xpZW50IjpmYWxzZSwiaXNQbHVzUGxhbiI6dHJ1ZSwiaWF0IjoxNzY1MjUxODcyLCJpc3MiOiJ1ZGFwaS1nYXRld2F5LXNlcnZpY2UiLCJleHAiOjE3NjUzMTc2MDB9.Gt-0IIsbkXkfA0_QnWW3FtCDY-rZQ-rV5ram4muzEWE"
 
 HEADERS = {
     "Accept": "application/json",
